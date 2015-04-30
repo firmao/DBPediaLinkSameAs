34cf1806e6ac5294f983c9c862b988640cd80fb4
Andre Valdestilhas <firmao@gmail.com>
2015-04-30 17:06:50 +0200

    Add table VOTE

--- CreateDB.sh
+++ CreateDB.sh
@@ -64,5 +64,6 @@ DBCONN="-h ${DBSERVER} -u ${DBUSER} --password=${DBPASSWORD}"
 echo "DROP DATABASE IF EXISTS ${DBNAME}" | mysql ${DBCONN}
 echo "CREATE DATABASE ${DBNAME}" | mysql ${DBCONN}
 echo "CREATE TABLE RAW_SAMEAS (dbpedia_uri varchar(750) DEFAULT NULL, link_target varchar(750) DEFAULT NULL, r_positive int(11) DEFAULT 0, r_negative int(11) DEFAULT 0)" | mysql $DBCONN $DBNAME
+echo "CREATE TABLE VOTE (dbpedia_uri varchar(750) DEFAULT NULL, link_target varchar(750) DEFAULT NULL, vote int(11) DEFAULT '0', suggestion varchar(750) DEFAULT NULL)" | mysql $DBCONN $DBNAME
 
 echo "LOAD DATA LOCAL INFILE 'ld.tsv' INTO TABLE RAW_SAMEAS FIELDS TERMINATED BY '\t'" | mysql $DBCONN $DBNAME --local-infile=1
